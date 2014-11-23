4fe1f213fe6a237fb77dd010051d5a5e6525ec70
Alexnava <alex.il.nava@gmail.com>
2014-11-23 19:14:00 +0100

    map centering

--- scripts/GeoCalc.js
+++ scripts/GeoCalc.js
@@ -1,7 +1,11 @@
-window.onload = function() {
-	this.geolocationAvailable = ('geolocation' in navigator)
-	this.pointsList = Array(0);
-};
+var posUpdate = function(position) {
+	// TODO: update point on map
+	currentPoint.x = position.coords.longitude;
+	currentPoint.y = position.coords.latitude;
+	currentPoint.radius = position.coords.accuracy;
+	
+	centerZoomMap();
+}
 
 var getLongitude = function(lonString){
 	
@@ -12,7 +16,9 @@ var getLatitude = function(latString){
 }
 
 var centerZoomMap = function(){
-	
+	var pos = new google.maps.LatLng(currentPoint.y, currentPoint.x);
+
+	map.setCenter(pos);
 }
 
 var calcAverage1 = function(){
@@ -46,5 +52,22 @@ var manageContinuous = function(){
 }
 
 document.getElementById('AddCurrent').onclick = addPoint;
-document.getElementById('AddContinuous').onsubmit = manageContinuous;
+document.getElementById('AddContinuous').onclick = manageContinuous;
 document.getElementById('AddManual').onsubmit = addPoint;
+
+var init = function() {
+	this.geolocationAvailable = ('geolocation' in navigator); // TODO: popup error
+	this.pointsList = Array(0);
+	this.pointCounter = 0; // For naming purpose, != from pointList.length
+	this.geoWatchID = navigator.geolocation.watchPosition(posUpdate);
+	this.currentPoint = {};
+	
+	var mapOptions = {
+		zoom: 6,
+		center: { lat: 45.5, lng: 9.2}
+	};
+
+	this.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+};
+
+google.maps.event.addDomListener(window, 'load', init);
